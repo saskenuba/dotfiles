7f9bbd490998cc964b8dc1dab4c5cdb16f8c7d43
Martin <martin@hotmail.com.br>
2022-08-25 10:35:44 -0300

    added clj snippets, function to reload dir-locals
    Signed-off-by: Martin <martin@hotmail.com.br>

--- spacemacs/private/martinmariano/funcs.el
+++ spacemacs/private/martinmariano/funcs.el
@@ -1,3 +1,19 @@
+(defun my-reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+
+(defun my-reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the
+current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (equal default-directory dir)
+          (my-reload-dir-locals-for-current-buffer))))))
+
 (defun dit-start-recording ()
   "Start screencast and F12 to adv presentation."
   (sha/screencast)
