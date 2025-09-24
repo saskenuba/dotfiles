25a3e70d4dd0b6783ddb681429d7e778d9a61bf2
Martin Mariano <martinmariano@Martins-MacBook-Pro.local>
2025-09-23 23:23:24 -0300

    macos changes

--- macos/.config/sketchybar/items/right/clock.sh
+++ macos/.config/sketchybar/items/right/clock.sh
@@ -4,6 +4,7 @@ clock=(
     update_freq=10
     icon="$CLOCK"
     icon.color="$CLOCK_ICON_COLOR"
+    icon.padding_right=4
     label.color="$CLOCK_LABEL_COLOR"
     script="$PLUGIN_DIR/clock.sh"
     click_script="open -a Clock"
